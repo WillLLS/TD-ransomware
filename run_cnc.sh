768ae43eb1bd28f9c08236b9c6cdca1952f0b8d0
WillLLS <williamlalis00@gmail.com>
2022-11-28 23:02:57 +0100

    add binary ransomware in CNC folders

--- run_cnc.sh
+++ run_cnc.sh
@@ -2,5 +2,7 @@ mkdir -p cnc_data
 docker run -it --rm --name cnc \
     --net=ransomware-network \
     -v "$PWD"/sources:/root/ransomware:ro \
+    -v "$PWD"/dist:/root/CNC/bin:ro \
     -v "$PWD"/cnc_data:/root/CNC ransomware \
     python /root/ransomware/cnc.py
+    
